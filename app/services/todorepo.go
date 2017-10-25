6da582f5b459174cab81c01c71be935ac57876b6
Nate Jenson <nate.m.jenson@gmail.com>
2017-10-24 19:26:04 -0500

    implement map-backed todo repository

--- app/services/todorepo.go
+++ app/services/todorepo.go
@@ -1,25 +1,62 @@
 package services
 
 import (
+	"errors"
+
 	"github.com/natejenson/go-api/app/models"
+	"github.com/nu7hatch/gouuid"
 )
 
 // TodoRepo is a repository for Todos
-type TodoRepo struct {
+type TodoRepo interface {
+	Get(id uuid.UUID) (*models.Todo, error)
+	GetAll() []*models.Todo
+}
+
+// InMemoryTodos represents a concurrent, in-memory repository of Todos
+type InMemoryTodos struct {
 	// Keep a *concurrent map of guid --> Todo
+	todos map[uuid.UUID]*models.Todo
 }
 
 // Get a Todo by ID
-func (r TodoRepo) Get(id int) models.Todo {
-	l := new(models.Todo)
-	l.ID = id
-	l.Done = false
-	l.Title = "Take Bear on a walk"
-	return *l
+func (r *InMemoryTodos) Get(id uuid.UUID) (*models.Todo, error) {
+	t, ok := r.todos[id]
+	if !ok {
+		return nil, errors.New("Todo not found")
+	}
+	return t, nil
 }
 
-// Get all the Todo
-func (r TodoRepo) GetAll() []models.Todo {
-	t := []models.Todo{r.Get(123), r.Get(456)}
-	return t
+// GetAll gets all the Todos
+func (r *InMemoryTodos) GetAll() []*models.Todo {
+	m := make([]*models.Todo, 0, len(r.todos))
+	for _, val := range r.todos {
+		m = append(m, val)
+	}
+	return m
+}
+
+// New creates an initialized todo repository
+func New() *InMemoryTodos {
+	a, _ := uuid.NewV4()
+	b, _ := uuid.NewV4()
+	c, _ := uuid.NewV4()
+	return &InMemoryTodos{
+		todos: map[uuid.UUID]*models.Todo{
+			*a: &models.Todo{ID: a.String(), Title: "Walk the dog"},
+			*b: &models.Todo{ID: a.String(), Title: "Go to Mars"},
+			*c: &models.Todo{ID: a.String(), Title: "Go outside"},
+		},
+	}
+}
+
+var todoRepo *InMemoryTodos
+
+// GetTodoRepo gets the TodoRepo singleton instance
+func GetTodoRepo() (r TodoRepo) {
+	if todoRepo == nil {
+		todoRepo = New()
+	}
+	return todoRepo
 }
